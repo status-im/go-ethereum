78b3337808c2da80e198f91d558c2377d0196358
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-06-10 18:14:17 -0500

    whisper bug fix for encrypted messages

--- whisper/whisper.go
+++ whisper/whisper.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
 	"github.com/ethereum/go-ethereum/event/filter"
 	"github.com/ethereum/go-ethereum/logger"
 	"github.com/ethereum/go-ethereum/logger/glog"
