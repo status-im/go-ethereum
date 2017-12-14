04e23dabc0c704c4b723fb1661a40c33e5a19b52
Evgeny Danienko <6655321@bk.ru>
2017-12-14 11:44:03 +0300

    eth: fmt
    Signed-off-by: Your Name <Your.Name@example.com>

--- eth/config.go
+++ eth/config.go
@@ -43,7 +43,7 @@ var DefaultConfig = Config{
 		DatasetsInMem:  1,
 		DatasetsOnDisk: 2,
 	},
-	ULC: &ULCConfig{MinTrustedFraction: 75},
+	ULC:           &ULCConfig{MinTrustedFraction: 75},
 	NetworkId:     1,
 	LightPeers:    100,
 	DatabaseCache: 768,
