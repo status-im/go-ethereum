86c40cbbf8709f323440522db71a0b5f37201c9e
b00ris <b00ris@mail.ru>
2018-06-04 14:06:20 +0300

    les: fmt

--- eth/config.go
+++ eth/config.go
@@ -84,8 +84,8 @@ type Config struct {
 	NoPruning bool
 
 	// Light client options
-	LightServ  int `toml:",omitempty"` // Maximum percentage of time allowed for serving LES requests
-	LightPeers int `toml:",omitempty"` // Maximum number of LES client peers
+	LightServ    int  `toml:",omitempty"` // Maximum percentage of time allowed for serving LES requests
+	LightPeers   int  `toml:",omitempty"` // Maximum number of LES client peers
 	OnlyAnnounce bool // Maximum number of LES client peers
 
 	// Ultra Light client options
