f29661d157f36a0f4936ec6df5feb51b2ef3e2ea
Adam Babik <a.babik@designfortress.com>
2019-09-26 00:19:00 +0200

    get rid of github.com/elastic/gosigar on mobile

--- metrics/cpu.go
+++ metrics/cpu.go
@@ -16,8 +16,6 @@
 
 package metrics
 
-import "github.com/elastic/gosigar"
-
 // CPUStats is the system and process CPU stats.
 type CPUStats struct {
 	GlobalTime int64 // Time spent by the CPU working on all processes
@@ -27,10 +25,5 @@ type CPUStats struct {
 
 // ReadCPUStats retrieves the current CPU stats.
 func ReadCPUStats(stats *CPUStats) {
-	global := gosigar.Cpu{}
-	global.Get()
-
-	stats.GlobalTime = int64(global.User + global.Nice + global.Sys)
-	stats.GlobalWait = int64(global.Wait)
-	stats.LocalTime = getProcessCPUTime()
+	readCPUStats(stats)
 }
