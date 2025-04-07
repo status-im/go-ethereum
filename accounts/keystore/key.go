95a0cb61664d1f1195104dfab80297cd09aed0df
Igor Sirotin <sirotin@status.im>
2025-04-07 12:47:03 +0100

    fix: imports to status-im

--- accounts/keystore/key.go
+++ accounts/keystore/key.go
@@ -32,7 +32,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/google/uuid"
-	"github.com/igor-sirotin/extkeys"
+	"github.com/status-im/extkeys"
 )
 
 const (
